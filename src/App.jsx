a14560f13bcd12f2a72c05bd58c1bf64494a3be6
Soysauce69 <anshika.32007@gmail.com>
2026-05-08 02:28:09 +0530

    Added new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Navbar from './components/Navbar'
-
+import Missions from './pages/Missions'
 import Dashboard from './pages/Dashboard'
 import Matches from './pages/Matches'
 import Analytics from './pages/Analytics'
+import Badges from './pages/Badges'
+import Social  from './pages/Social'
+import Profile from './pages/Profile'
 
 export default function App() {
   return (
@@ -13,15 +16,30 @@ export default function App() {
 
       <Routes>
         <Route path="/" element={<Dashboard />} />
-
+        <Route
+          path="/badges"
+          element={<Badges />}
+        />
+        <Route
+          path="/missions"
+          element={<Missions />}
+        />
         <Route
           path="/matches"
           element={<Matches />}
+        />
+        <Route
+          path="/social"
+          element={<Social />}
         />
 
         <Route
           path="/analytics"
           element={<Analytics />}
+        />
+        <Route
+          path="/profile"
+          element={<Profile />}
         />
       </Routes>
     </div>
